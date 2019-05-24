6e10b3672c9ef72928b275f6cda1372c6c0a2e42
xiaxinkai <xiaxinkai@gmail.com>
2019-05-24 23:46:55 +0800

    Add UITextField and UITextField

--- iOSPHF/ViewController.swift
+++ iOSPHF/ViewController.swift
@@ -13,8 +13,17 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let textField = UITextField(frame: CGRect(x:100, y:500, width:200, height:30))
+        textField.borderStyle = .line
+        textField.placeholder = "请输入文字"
+        textField.clearButtonMode = .always
+        self.view.addSubview(textField)
+        
+        let label = UILabel(frame: CGRect(x:100, y:650, width:200, height:30))
+        label.backgroundColor = UIColor.green
+        label.text = "我是一个标签"
+        self.view.addSubview(label)
     }
-
-
 }
 
